2a65a6a1359b7fc06e5fb852a599251a2431d4a3
Anju-UST <Anju.Reji@ust.com>
2023-07-04 21:56:35 +0530

    application

---
+++ UST_Project_Files-main/Frontend/FoodApp/src/app/login/login.component.ts
@@ -0,0 +1,98 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Login } from '../modal/login';
+import { AuthenticateServiceService } from '../service/authenticate-service.service';
+import { RouterServiceService } from '../service/router-service.service';
+import { UserAuthService } from '../authservice/user-auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  login: Login = new Login();
+  loginForm: FormGroup;
+  submitMessage!: string;
+  flag: boolean = false;
+  usertype!: string;
+  userid!: any;
+  myid!: any;
+  private isAuthenticated = false;
+
+  constructor(
+    private routerService: RouterServiceService,
+    private authservice: AuthenticateServiceService,
+    private route: Router,
+    private userAuthService:UserAuthService
+  ) {
+    this.loginForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+    });
+  }
+
+  ngOnInit(): void {
+    this.isAuthenticated = !!localStorage.getItem('token');
+    this.authservice.retrieveUserId();
+  }
+
+  onSubmit() {
+    this.login.userName = this.loginForm.value.username;
+    this.login.userPassword = this.loginForm.value.password;
+
+    this.submitMessage = this.loginForm.value.username;
+
+    this.authservice.getusers(this.login).subscribe(
+      (data: any) => {
+        this.authservice.setBearerToken(data['token']);
+        if (data != null) {
+          localStorage.setItem('token', this.submitMessage);
+          localStorage.setItem('username',this.login.userName);
+          this.flag = true;
+          this.setAuthenticated(true);
+          this.userid = data.user.userid;
+          localStorage.setItem('myuseridd', this.userid);
+
+          this.userAuthService.setRoles(data.user.role);
+          this.userAuthService.setToken(data.jwtToken);
+
+          const role = data.user.role[0].roleName;
+          if(role==='User'){
+            this.route.navigate(['/user']);
+          }
+          else{
+            alert('You are not authorized to Login');
+          }
+        }
+      },
+      (error) => {
+        console.log('error');
+        alert('You have entered incorrect username or password!');
+      }
+    );
+  }
+
+  isAuthenticatedUser() {
+    return this.isAuthenticated;
+  }
+
+  setAuthenticated(status: boolean): void {
+    this.isAuthenticated = status;
+  }
+
+  logout() {
+    this.setAuthenticated(false);
+    localStorage.removeItem('token');
+    this.route.navigate(['/login']);
+  }
+
+  idnum = localStorage.getItem('myuseridd');
+
+  // sendidtoservice(){
+  //     return this.authservice.retrieveUserId(this.idnum);
+  // }
+
+  
+}
---
+++ UST_Project_Files-main/Frontend/FoodApp/src/app/service/authenticate-service.service.ts
@@ -0,0 +1,85 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { map } from 'rxjs/operators';
+import { Login } from '../modal/login';
+import { Signup } from '../modal/signup';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticateServiceService {
+
+  private authenticated: boolean = false;
+  private tokenStr!: string;
+  userid!: any;
+
+  constructor(private httpClient: HttpClient, private route: Router) {
+    this.checkAuthentication();
+  }
+
+  addUser(signup: Signup): Observable<Signup> {
+    return this.httpClient.post<Signup>('http://localhost:8088/api/auth/registerNewUser', signup);
+  }
+
+  addKitchen(signup: Signup): Observable<Signup> {
+    return this.httpClient.post<Signup>(`http://localhost:8088/api/auth/registerNewKitchenStaff`, signup);
+  }
+
+  getusers(userr: Login) {
+    console.log("GET USER");
+    console.log(userr.userName);
+    console.log(userr.userPassword);
+    this.authenticated = true;
+    return this.httpClient.post<any>(`http://localhost:8088/api/auth/authenticate`, userr, { headers: new HttpHeaders().set('responseType', 'text') }).pipe(
+      map(
+        userData => {
+          localStorage.setItem('username', userr.userName);
+          this.tokenStr = userData.token;
+          console.log("Token string: " + this.tokenStr);
+          localStorage.setItem('token', this.tokenStr);
+          const id = userData.userid;
+          console.log("my id is " + id);
+          localStorage.setItem('myuseridd', id);
+          this.userid = id;
+          return userData;
+        }
+      )
+    );
+  }
+
+  getUserByUsername(username: string) {
+    return this.httpClient.get<any>(`http://localhost:8088/api/user/details/${username}`);
+  }
+
+  isAuthenticated(): boolean {
+    return this.authenticated;
+  }
+
+  setAuthenticated(status: boolean): void {
+    this.authenticated = status;
+  }
+
+  logout() {
+    this.authenticated = false;
+    localStorage.removeItem('token');
+    localStorage.removeItem('myuseridd');
+    this.route.navigate(['/user']);
+  }
+
+  setBearerToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  private checkAuthentication() {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('myuseridd');
+    this.authenticated = !!token && !!userId;
+    this.userid = userId;
+  }
+
+  retrieveUserId() {
+    return this.userid;
+  }
+}
